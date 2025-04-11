e9ad8faca18dea5e956b0807c3b5b72edf90012e
Valerie Nova <10227076+novalastix@users.noreply.github.com.>
2025-04-10 21:21:11 -0700

    fill hexes and disappear on click

--- game.js
+++ game.js
@@ -1,11 +1,56 @@
 console.log("Game Begin");
 
+const boards=
+[
+  [
+    "c6"
+  ],
+  [
+    "c1","c6"
+  ],
+  [
+    "c1","c6","c11"
+  ],
+  [
+    "c1","c3","c4","c6"
+  ],
+  [
+    "c3","c4","c6","c8","c9"
+  ],
+  [
+    "c1","c3","c4","c8","c9","c11"
+  ],
+  [
+    "c1","c3","c4","c6","c8","c9","c11"
+  ],
+  [
+    "c1","c3","c4","c5","c7","c8","c9","c11"
+  ],
+  [
+    "c1","c3","c4","c5","c6","c7","c8","c9","c11"
+  ],
+  [
+    "c0","c1","c2","c3","c4","c5","c7","c6","c8","c9"
+  ],
+  [
+    "c0","c1","c2","c3","c4","c5","c7","c6","c8","c9","c11"
+  ],
+  [
+    "c0","c1","c2","c3","c4","c5","c7","c8","c9","c10","c11","c12"
+  ],
+  [
+    "c0","c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12"
+  ]
+];
+
+let currentPlayers = 0;
+
 function loadGame()
 {
   players = getPlayersArray();
   console.log(players)
-
-  
+  currentPlayers = players.length;
+  fillTiles(players)
 }
 
 function getJsonFromUrl(url) {
@@ -32,13 +77,25 @@ function getPlayersArray(){
   return players;
 }
 
-function placeTiles()
+function checkScore(){
+
+}
+
+function fillTiles(players)
 {
-  for(var i=0;i<13;i++)
+  let board = boards[players.length - 1];
+  for(let i = 0;i<players.length;i++)
   {
-    const cell = document.createElement("div");
-    cell.classList.push("cell");
-    cell.id = "c"+i;
+    let hexId = board[i];
+    let hex = document.getElementById(hexId);
+    hex.style.display = "block";
+    hex.childNodes[0].textContent = players[i];
+
+    hex.childNodes[0].addEventListener("click",function(){
+      hex.classList.add("dead");
+      currentPlayers--;
+      checkScore();
+    })
   }
 }
 
